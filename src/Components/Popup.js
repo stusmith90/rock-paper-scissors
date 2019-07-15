c17dec757729db6105832a2a0a1c8db333792203
Stuart Smith <stuart.ingersoll-smith@cherrythinking.com>
2019-07-15 20:22:27 +0100

    add popup functionality

---
+++ src/Components/Popup.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+class Popup extends React.Component {
+  render() {
+    return (
+      <div className="popup">
+        <div className="popup\_inner">
+          <p>player Choice: {this.props.playerChoice}</p>
+          <p>Computer Choice: {this.props.computerChoice}</p>
+          <button onClick={this.props.handleClick}>close me</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Popup;
